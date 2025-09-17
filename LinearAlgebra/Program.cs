abd07f9369048b7f05f80bfb8733ece511beade8
Yuhan Zhao <aidenzhao@hotmail.com>
2025-09-16 20:59:18 -0400

    Added Vector class

--- LinearAlgebra/Program.cs
+++ LinearAlgebra/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Vector myVec1 = new(1, 2, 3);
+Vector myVec2 = new(4, 5, 6);
+Console.WriteLine(myVec1 / myVec2);
---
+++ LinearAlgebra/Vector.cs
@@ -0,0 +1,42 @@
+using System.Numerics;
+
+public class Vector
+{
+  float x, y, z;
+
+  public Vector(float a, float b, float c)
+  {
+    x = a; y = b; z = c;
+  }
+
+  public static Vector operator +(Vector a, Vector b)
+  {
+    return new Vector(a.x + b.x, a.y + b.y, a.z + b.z);
+  }
+
+  public static Vector operator -(Vector a, Vector b)
+  {
+    return new Vector(a.x - b.x, a.y - b.y, a.z - b.z);
+  }
+
+  public static float operator *(Vector a, Vector b)
+  {
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+  }
+
+  public static Vector operator /(Vector a, Vector b)
+  {
+    return new Vector(
+      a.y * b.z - a.z * b.y,
+      a.z * b.x - a.x * b.z,
+      a.x * b.y - a.y * b.x
+    );
+  }
+
+
+
+  override public String ToString()
+  {
+    return $"[{x}, {y}, {z}]";
+  }
+}
